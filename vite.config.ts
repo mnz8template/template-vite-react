c5754c879e6b190e5a561248460aaa4cc0cb476d
line126 <line112266@126.com>
2023-04-29 23:38:17 +0800

    vite plugin react_convention_route

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { react_convention_route } from './plugins';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), react_convention_route()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://xxx.xxx.com',
+  //       changeOrigin: true,
+  //       // rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 });
